4f1d82f383f7f182780fb5fa9e040db1aaed04c5
benthecarman <benthecarman@live.com>
2024-02-20 12:26:21 +0000

    Return chess winner, add apply move function

--- chess/src/lib.rs
+++ chess/src/lib.rs
@@ -1,31 +1,56 @@
 use anyhow::anyhow;
 use extism_pdk::*;
 use pleco::bot_prelude::*;
-use pleco::Board;
+use pleco::{BitMove, Board};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Params {
+pub struct NextMoveParams {
     pub board: String,
     pub algo: Option<String>,
     pub depth: Option<u16>,
 }
 
 #[plugin_fn]
-pub fn calculate_next_move(Json(params): Json<Params>) -> FnResult<String> {
+pub fn calculate_next_move(Json(params): Json<NextMoveParams>) -> FnResult<String> {
     let board =
         Board::from_fen(&params.board).map_err(|e| anyhow!("Error parsing board: {e:?}"))?;
 
     let depth = params.depth.unwrap_or(1);
     let chess_move = match params.algo.as_deref() {
-        None => IterativeSearcher::best_move(board, depth),
-        Some("random") => RandomBot::best_move(board, depth),
-        Some("minimax") => MiniMaxSearcher::best_move(board, depth),
-        Some("parallel-minimax") => ParallelMiniMaxSearcher::best_move(board, depth),
-        Some("jamboree") => JamboreeSearcher::best_move(board, depth),
-        Some("iterative") => IterativeSearcher::best_move(board, depth),
+        None => IterativeSearcher::best_move(board.clone(), depth),
+        Some("random") => RandomBot::best_move(board.clone(), depth),
+        Some("minimax") => MiniMaxSearcher::best_move(board.clone(), depth),
+        Some("parallel-minimax") => ParallelMiniMaxSearcher::best_move(board.clone(), depth),
+        Some("jamboree") => JamboreeSearcher::best_move(board.clone(), depth),
+        Some("iterative") => IterativeSearcher::best_move(board.clone(), depth),
         Some(algo) => return Err(anyhow!("Unknown algo: {algo}").into()),
     };
 
-    Ok(chess_move.stringify())
+    if chess_move.get_raw() == 0 && board.checkmate() {
+        let turn = board.turn().other_player().to_string();
+        return Ok(json!({"winner": turn}).to_string());
+    }
+
+    let json = json!({"move": chess_move.stringify(), "data": chess_move.get_raw()});
+    Ok(json.to_string())
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ApplyMoveParams {
+    pub board: String,
+    #[serde(rename = "move")]
+    pub chess_move: u16,
+}
+
+#[plugin_fn]
+pub fn apply_move(Json(params): Json<ApplyMoveParams>) -> FnResult<String> {
+    let mut board =
+        Board::from_fen(&params.board).map_err(|e| anyhow!("Error parsing board: {e:?}"))?;
+
+    let bitmove = BitMove::new(params.chess_move);
+    board.apply_move(bitmove);
+
+    Ok(board.fen())
 }
--- even_block_hash/src/lib.rs
+++ even_block_hash/src/lib.rs
@@ -2,24 +2,24 @@ use extism_pdk::*;
 
 #[plugin_fn]
 pub fn is_latest_block_hash_even() -> FnResult<String> {
-	let input = HttpRequest {
-		url: "https://mempool.space/api/blocks/tip/hash".into(),
-		headers: Default::default(),
-		method: None,
-	};
-	let res = http::request::<()>(&input, None)?;
-	let body = res.body();
-	let string = String::from_utf8(body)?;
+    let input = HttpRequest {
+        url: "https://mempool.space/api/blocks/tip/hash".into(),
+        headers: Default::default(),
+        method: None,
+    };
+    let res = http::request::<()>(&input, None)?;
+    let body = res.body();
+    let string = String::from_utf8(body)?;
 
-	info!("got block {string}");
+    info!("got block {string}");
 
-	let bytes = hex::decode(&string)?;
-	let last = bytes.last().unwrap();
+    let bytes = hex::decode(&string)?;
+    let last = bytes.last().unwrap();
 
-	info!("last: {last}");
-	if last % 2 == 0 {
-		Ok("true".to_string())
-	} else {
-		Ok("false".to_string())
-	}
+    info!("last: {last}");
+    if last % 2 == 0 {
+        Ok("true".to_string())
+    } else {
+        Ok("false".to_string())
+    }
 }
